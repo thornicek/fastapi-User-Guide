ee0d6057311451725cf5769792b6db13b1df1a6a
Tomas Hornicek <tomas@MacBookPro.attlocal.net>
2025-01-08 15:12:17 -0500

    Request Body Completed

--- RequestBody.py
+++ RequestBody.py
@@ -3,14 +3,20 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
-    name:str
+    name: str
     description: Union[str, None] = None
-    price : float
+    price: float
     tax: Union[float, None] = None
+
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
