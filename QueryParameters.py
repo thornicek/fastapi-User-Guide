3da216f3a35f6bc2bab31f5b952e80faaf18fbc0
Tomas Hornicek <tomas@MacBookPro.attlocal.net>
2025-01-08 14:12:04 -0500

    Query parameters with type conversion

--- QueryParameters.py
+++ QueryParameters.py
@@ -4,8 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None):
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q" : q}
-    return {"item_id" : item_id}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
