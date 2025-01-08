2edbb54ebff3be57b655fbd8b0d585bfc255c7f4
Tomas Hornicek <tomas@MacBookPro.attlocal.net>
2025-01-08 14:22:55 -0500

    Query Parameters Completed

--- PathParameters.py
+++ PathParameters.py
@@ -17,4 +17,4 @@ async def get_model(model_name : ModelName):
     if model_name.value == "lenet":
         return {"model_name" : model_name, "message" : "LeCNN all the images"}
 
-    return {"model_name" : model_name, "message": "Have some residualsGr"}
+    return {"model_name" : model_name, "message": "Have some residuals"}
--- QueryParameters.py
+++ QueryParameters.py
@@ -6,12 +6,8 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
