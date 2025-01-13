259bf220627934145bb1c4cc6a1e29ae670c6a61
thornicek <tomas.hornicek8@email.cz>
2025-01-13 12:25:35 -0500

    Bodies of arbitary dict completed

--- BodyNestedModels.py
+++ BodyNestedModels.py
@@ -1,16 +1,11 @@
-from typing import List
+from typing import Dict
 
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    return images
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
