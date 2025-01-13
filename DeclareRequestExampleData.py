4d088b9f09650391428ec771378c252d150c3a55
thornicek <tomas.hornicek8@email.cz>
2025-01-13 15:00:36 -0500

    Body with examples

--- DeclareRequestExampleData.py
+++ DeclareRequestExampleData.py
@@ -1,19 +1,34 @@
-from typing import Union
+from typing import Annotated, Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi import Body,FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str = Field(examples=["Foo"])
-    description: Union[str, None] = Field(default=None, examples=["A very nice Item"])
-    price: float = Field(examples=[35.4])
-    tax: Union[float, None] = Field(default=None, examples=[3.2])
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+@app.put("/item/{item_id}")
+async def update_item(
+        item_id: int,
+        item: Annotated[
+            Item,
+            Body(
+                examples=[
+                    {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    }
+                ]
+            )
+        ]
+):
     results = {"item_id": item_id, "item": item}
     return results
