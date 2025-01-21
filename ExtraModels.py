fa66cd647812ffe48d17fce4f0c90dcbf0d4ddd0
thornicek <tomas.hornicek8@email.cz>
2025-01-21 11:46:07 -0500

    Response with arbitary dict

--- ExtraModels.py
+++ ExtraModels.py
@@ -1,22 +1,10 @@
-from typing import List
+from typing import Dict
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str
-
-
-items = [
-    {"name": "Foo", "description": "There comes my hero"},
-    {"name": "Red", "description": "It's my aeroplane"},
-]
-
-
-@app.get("/items/", response_model=List[Item])
-async def read_items():
-    return items
+@app.get("/keyword-weights/", response_model=Dict[str, float])
+async def read_keyword_weights():
+    return {"foo": 2.3, "bar": 3.4}
