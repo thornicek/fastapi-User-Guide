9970b74721b434bcdd6a4c502ad767c3d89a4808
thornicek <tomas.hornicek8@email.cz>
2025-01-21 08:17:12 -0500

    Disable response model

--- ResponseModelReturnType.py
+++ ResponseModelReturnType.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, Response
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
-@app.get("/teleport")
-async def get_teleport() -> RedirectResponse:
-    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+@app.get("/portal", response_model=None)
+async def get_portal(teleport: bool = False) -> Union[Response, dict]:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
