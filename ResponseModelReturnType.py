34797234dba0ef38df981e62bc3e997f8f78423f
thornicek <tomas.hornicek8@email.cz>
2025-01-21 08:15:55 -0500

    Annotate a response subclass

--- ResponseModelReturnType.py
+++ ResponseModelReturnType.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI, Response
-from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
-@app.get("/portal")
-async def get_portal(teleport: bool = False) -> Response:
-    if teleport:
-        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    return JSONResponse(content={"message": "Here's your interdimensional portal."})
+@app.get("/teleport")
+async def get_teleport() -> RedirectResponse:
+    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
