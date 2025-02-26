4a669d4afdfab4ce30b1147144531ff560fa5af1
thornicek <tomas.hornicek8@email.cz>
2025-02-26 16:43:31 -0500

    Form Models Completed

--- FormModels.py
+++ FormModels.py
@@ -5,9 +5,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class FormData(BaseModel):
     username: str
     password: str
+    model_config = {"extra": "forbid"}
+
 
 @app.post("/login/")
 async def login(data: Annotated[FormData, Form()]):
