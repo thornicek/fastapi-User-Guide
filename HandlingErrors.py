da2bae46f890ae039f6bbe90880d086baeb015ae
thornicek <tomas.hornicek8@email.cz>
2025-03-10 15:14:07 -0400

    Add custom headers

--- HandlingErrors.py
+++ HandlingErrors.py
@@ -5,8 +5,12 @@ app = FastAPI()
 items = {"foo": "The Foo Wrestlers"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str):
     if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
     return {"item": items[item_id]}
