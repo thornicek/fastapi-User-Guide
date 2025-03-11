0c015fe6430d8874f3947af49f443efcf4a45fc8
thornicek <tomas.hornicek8@email.cz>
2025-03-11 10:52:43 -0400

    Summary and description

--- PathOperationConfiguration.py
+++ PathOperationConfiguration.py
@@ -1,20 +1,23 @@
-from enum import Enum
-
+from typing import Union, Set
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Tags(Enum):
-    items = "items"
-    users = "users"
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: set[str] = set()  # Pydantic v2 supports this
 
 
-@app.get("/items/", tags=[Tags.items])
-async def get_items():
-    return ["Portal gun", "Plumbus"]
-
-
-@app.get("/users/", tags=[Tags.users])
-async def read_users():
-    return ["Rick", "Morty"]
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+)
+async def create_item(item: Item):
+    return item
