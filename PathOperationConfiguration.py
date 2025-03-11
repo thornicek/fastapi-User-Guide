34e1ec993888151f2c14c5bf48730da392b09dc8
thornicek <tomas.hornicek8@email.cz>
2025-03-11 10:59:20 -0400

    Path Operations Configuration

--- PathOperationConfiguration.py
+++ PathOperationConfiguration.py
@@ -1,4 +1,5 @@
-from typing import Union, Set
+from typing import Union
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -10,14 +11,18 @@ class Item(BaseModel):
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: set[str] = set()  # Pydantic v2 supports this
+    tags: set[str] = set()
 
 
-@app.post(
-    "/items/",
-    response_model=Item,
-    summary="Create an item",
-    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
-)
+@app.post("/items/", response_model=Item, summary="Create an item")
 async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
     return item
