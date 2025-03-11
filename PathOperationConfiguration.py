31d26c3a15011295b814289ea6c47dee2d7c164f
thornicek <tomas.hornicek8@email.cz>
2025-03-11 11:02:26 -0400

    Deprecate a path operation

--- PathOperationConfiguration.py
+++ PathOperationConfiguration.py
@@ -1,33 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: set[str] = set()
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
 
 
-@app.post(
-    "/items/",
-    response_model=Item,
-    summary="Create an item",
-    response_description="The created item",
-)
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
 
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
+
+@app.get("/elements/", tags=["items"], deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
