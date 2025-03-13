9e5ba3f91cdcd5441a408e8c08d9344770a9a9af
thornicek <tomas.hornicek8@email.cz>
2025-03-13 15:57:19 -0400

    Dependencies with yield and except

--- Dependencies/DependenciesWithYield.py
+++ Dependencies/DependenciesWithYield.py
@@ -5,28 +5,25 @@ from fastapi import Depends, FastAPI, HTTPException
 app = FastAPI()
 
 
-data = {
-    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
-    "portal-gun": {"description": "Gun to create portals", "owner": "Rick"},
-}
-
-
-class OwnerError(Exception):
+class InternalError(Exception):
     pass
 
 
 def get_username():
     try:
         yield "Rick"
-    except OwnerError as e:
-        raise HTTPException(status_code=400, detail=f"Owner error: {e}")
+    except InternalError:
+        print("Oops, we didn't raise again, Britney 😱")
 
 
 @app.get("/items/{item_id}")
 def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
-    if item_id not in data:
-        raise HTTPException(status_code=404, detail="Item not found")
-    item = data[item_id]
-    if item["owner"] != username:
-        raise OwnerError(username)
-    return item
+    if item_id == "portal-gun":
+        raise InternalError(
+            f"The portal gun is too dangerous to be owned by {username}"
+        )
+    if item_id != "plumbus":
+        raise HTTPException(
+            status_code=404, detail="Item not found, there's only a plumbus here"
+        )
+    return item_id
