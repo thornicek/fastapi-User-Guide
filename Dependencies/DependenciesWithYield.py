ac3aaab79c58c9b47d7231afde2f6953186aafdb
thornicek <tomas.hornicek8@email.cz>
2025-03-13 15:53:35 -0400

    Dependencies with Yield and HTTPException

--- Dependencies/DependenciesWithYield.py
+++ Dependencies/DependenciesWithYield.py
@@ -1,27 +1,32 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, FastAPI, HTTPException
+
+app = FastAPI()
 
 
-async def dependency_a():
-    dep_a = generate_dep_a()
+data = {
+    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
+    "portal-gun": {"description": "Gun to create portals", "owner": "Rick"},
+}
+
+
+class OwnerError(Exception):
+    pass
+
+
+def get_username():
     try:
-        yield dep_a
-    finally:
-        dep_a.close()
+        yield "Rick"
+    except OwnerError as e:
+        raise HTTPException(status_code=400, detail=f"Owner error: {e}")
 
 
-async def dependency_b(dep_a: Annotated[DepA, Depends(dependency_a)]):
-    dep_b = generate_dep_b()
-    try:
-        yield dep_b
-    finally:
-        dep_b.close(dep_a)
-
-
-async def dependency_c(dep_b: Annotated[DepB, Depends(dependency_b)]):
-    dep_c = generate_dep_c()
-    try:
-        yield dep_c
-    finally:
-        dep_c.close(dep_b)
+@app.get("/items/{item_id}")
+def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+    if item_id not in data:
+        raise HTTPException(status_code=404, detail="Item not found")
+    item = data[item_id]
+    if item["owner"] != username:
+        raise OwnerError(username)
+    return item
